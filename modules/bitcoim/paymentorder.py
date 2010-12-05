64ed096422ccce91339bbfb4cc556d5743c4c44f
davux <da@weeno.net>
2010-12-05 16:47:51 -0600

    Construct a PaymentOrder from existing payment

--- modules/bitcoim/paymentorder.py
+++ modules/bitcoim/paymentorder.py
@@ -6,14 +6,40 @@ import random
 class PaymentOrder(object):
     '''A payment order.'''
 
-    def __init__(self, from_jid, address, amount, comment='', fee=0, code=None, date=None):
+    def __init__(self, from_jid, address=None, amount=None, comment='', fee=0, code=None):
         self.jid = from_jid
-        self.address = address
-        self.amount = amount
-        self.comment = comment
-        self.fee = fee
-        self.code = code
-        self.date = date
+        if code is None:
+            self.address = address
+            self.amount = amount
+            self.comment = comment
+            self.fee = fee
+            self.date = None
+        else:
+            self.code = code
+            condition = 'from_jid=? and code=?'
+            values = [from_jid, code]
+            if address is not None:
+                condition += ' and recipient=?'
+                values.append(address)
+            if amount is not None:
+                condition += ' and amount=?'
+                values.append(amount)
+            if comment is not None:
+                condition += ' and comment=?'
+                values.append(comment)
+            if fee is not None:
+                condition += ' and fee=?'
+                values.append(fee)
+            req = 'select %s, %s, %s, %s, %s from %s where %s' % \
+                  ('date', 'recipient', 'amount', 'comment', 'fee', \
+                   'payments', condition)
+            SQL().execute(req, tuple(values))
+            paymentOrder = SQL().fetchone()
+            if paymentOrder is None:
+                raise PaymentNotFoundError
+            else:
+                (self.date, self.address, self.amount, \
+                 self.comment, self.fee) = tuple(paymentOrder)
 
     @staticmethod
     def genConfirmationCode(length=4, alphabet='abcdefghjkmnpqrstuvwxyz23456789'):
@@ -32,3 +58,6 @@ class PaymentOrder(object):
         req = 'insert into %s (%s, %s, %s, %s, %s, %s, %s) values (?, ?, ?, ?, ?, ?, ?)' % \
               ('payments', 'from_jid', 'date', 'recipient', 'amount', 'comment', 'confirmation_code', 'fee')
         SQL().execute(req, (self.jid, self.date, self.address, self.amount, self.comment, self.code, self.fee))
+
+class PaymentNotFoundError(Exception):
+    '''The requested payment was not found.'''
